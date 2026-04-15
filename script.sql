7777da70957acabfc88aa348816dfcce4dc10336
hamza aballagh <hamzaaballagh941@gmail.com>
2026-04-15 09:55:26 +0100

    creat table users and add the foreign key with roles

--- script.sql
+++ script.sql
@@ -3,3 +3,12 @@ CREATE TABLE  roles(
     id INT PRIMARY KEY AUTO_INCREMENT,
     label VARCHAR (255)
 );
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT ,
+    firstname VARCHAR(255) NOT NULL,
+    lastename VARCHAR (255) NOT NULL,
+    email VARCHAR (255) UNIQUE ,
+    password VARCHAR(255) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+);
