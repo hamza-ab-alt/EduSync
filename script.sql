8b94e2262d35181f3c45171d2854533ef2801e6c
hamza aballagh <hamzaaballagh941@gmail.com>
2026-04-16 10:26:20 +0100

    create table courses and add foreign key with users

--- script.sql
+++ script.sql
@@ -26,3 +26,11 @@ CREATE TABLE students (
     class_id INT ,
     FOREIGN KEY (class_id) REFERENCES classes(id)
 );
+CREATE TABLE courses(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR (255) NOT NULL,
+    total_hours INT,
+    professor_id INT,
+    FOREIGN KEY (professor_id) REFERENCES users(id)
+);
